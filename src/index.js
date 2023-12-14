57e0bd4989deb66ec5385976f3a5aae26c6f6e02
Kodam-Vinay <vinay.kodam112@gmail.com>
2023-12-14 20:16:45 +0530

    established socket.io

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+dotenv.config();
+const io = new Server({ cors: "http://localhost:3000/" });
+const PORT = process.env.PORT || 8000;
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+  //online users
+  socket.on("addNewUser", (userId) => {
+    !onlineUsers.some((user) => user.userId === userId) &&
+      onlineUsers.push({
+        userId,
+        socketId: socket.id,
+      });
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+
+  //send message
+  socket.on("sendMessage", (message) => {
+    const user = onlineUsers.find(
+      (user) => user?.userId === message?.recipientId
+    );
+
+    user && io.to(user.socketId).emit("getMessage", message);
+  });
+
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user?.socketId !== socket.id);
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+});
+
+io.listen(PORT);
